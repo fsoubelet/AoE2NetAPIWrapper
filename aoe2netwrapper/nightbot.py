abda676b127e325debc998a323e6580d1738068b
Felix Soubelet <felix.soubelet@protonmail.com>
2021-01-09 00:02:22 +0100

    improved docstring

--- aoe2netwrapper/nightbot.py
+++ aoe2netwrapper/nightbot.py
@@ -16,8 +16,8 @@ from aoe2netwrapper.exceptions import NightBotException
 
 class AoE2NightbotAPI:
     """
-    The 'AoE2NightbotAPI' class is a client that encompasses the https://aoe2.net/#nightbot
-    Nightbot API functions, which only return their requested data as plain text.
+    The 'AoE2NightbotAPI' class is a client that encompasses the https://aoe2.net/#nightbot API endpoints,
+    which only return their requested data as plain text.
     """
 
     NIGHTBOT_BASE_URL: str = "https://aoe2.net/api/nightbot"
