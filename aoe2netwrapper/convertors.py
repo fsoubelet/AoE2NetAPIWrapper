d56a5a0ae429f5b5f93ee50fd77814a966271c17
Felix Soubelet <felix.soubelet@protonmail.com>
2021-01-14 21:07:53 +0100

    check pandas is installed, added docstrings and lobbies method

--- aoe2netwrapper/convertors.py
+++ aoe2netwrapper/convertors.py
@@ -1,4 +1,6 @@
-import pandas as pd
+from typing import List
+
+from loguru import logger
 
 from aoe2netwrapper.models import (
     LastMatchResponse,
@@ -8,18 +10,38 @@ from aoe2netwrapper.models import (
     RatingTimePoint,
     StringsResponse,
 )
-from loguru import logger
+
+try:
+    import pandas as pd
+except ImportError:
+    logger.error(
+        "The 'aoe2netwrapper.convertors' module exports results to 'pandas.DataFrame' objects and "
+        "needs the 'pandas' library installed to function."
+    )
+    raise NotImplementedError("The 'pandas' library is required but not installed.")
 
 
 class Convert:
     """
     This is a convenience class providing methods to convert the outputs from the AoE2NetAPI query methods
-    into pandas DataFrame objects.
+    into pandas DataFrame objects. Every method below is a staticmethod, so no object has to be instantiated.
     """
 
     @staticmethod
     def leaderboard(leaderboard_response: LeaderBoardResponse) -> pd.DataFrame:
-        logger.debug("Converting LeaderBoardResponse leadrboard to DataFrame")
+        """
+        Convert the result given by a call to AoE2NetAPI().leaderboard to a pandas DataFrame.
+
+        Args:
+            leaderboard_response (LeaderBoardResponse): the response directly returned by your AoE2NetAPI
+                client.
+
+        Returns:
+            A pandas DataFrame from the LeaderBoardResponse, each row being an entry in the leaderboard.
+            Top level attributes such as 'start' or 'total' are broadcast to an entire array the size of
+            the dataframe.
+        """
+        logger.debug("Converting LeaderBoardResponse leaderboard to DataFrame")
         dframe = pd.DataFrame(leaderboard_response.leaderboard)
 
         logger.trace("Exporting attributes to columns and removing duplicate data")
@@ -40,7 +62,44 @@ class Convert:
         return dframe
 
     @staticmethod
+    def lobbies(lobbies_response: List[MatchLobby]) -> pd.DataFrame:
+        """
+        Convert the result given by a call to AoE2NetAPI().lobbies to a pandas DataFrame.
+
+        Args:
+            lobbies_response (List[MatchLobby]): the response directly returned by your AoE2NetAPI
+                client.
+
+        Returns:
+            A pandas DataFrame from the list of MatchLobby elements, each row being the information from
+            one MatchLobby in the list. Beware: the 'players' column is directly the content of the
+            'MatchLobby.players' attribute and as such holds a list of LobbyMember objects.
+        """
+        logger.debug("Converting Lobbies response to DataFrame")
+        dframe = pd.DataFrame(lobbies_response)
+
+        logger.trace("Exporting attributes to columns and removing duplicate data")
+        for i in range(41):
+            attribute = dframe[i][0][0]
+            dframe[attribute] = dframe[i].apply(lambda x: x[1])
+            del dframe[i]
+
+        return dframe
+
+    @staticmethod
     def num_online(num_online_response: NumOnlineResponse) -> pd.DataFrame:
+        """
+        Convert the result given by a call to AoE2NetAPI().num_online to a pandas DataFrame.
+
+        Args:
+            num_online_response (NumOnlineResponse): the response directly returned by your AoE2NetAPI
+                client.
+
+        Returns:
+            A pandas DataFrame from the NumOnlineResponse, each row being an entry in the leaderboard.
+            Top level attributes such as 'app_id' are broadcast to an entire array the size of the
+            dataframe, and timestamps are converted to datetime objects.
+        """
         logger.debug("Converting NumOnlineResponse to DataFrame")
         dframe = pd.DataFrame(num_online_response.dict())
 
