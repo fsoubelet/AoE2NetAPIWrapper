cc774fc1d9f5b2f3af7a3362200395188a4f6132
Felix Soubelet <felix.soubelet@protonmail.com>
2021-01-19 13:47:45 +0100

    no qa on MatchLobby duplicate export

--- aoe2netwrapper/models/matches.py
+++ aoe2netwrapper/models/matches.py
@@ -6,4 +6,4 @@ This module contains the model objects to encapsulate the responses from the end
 https://aoe2.net/api/matches
 """
 # pylint: disable=unused-import
-from aoe2netwrapper.models.lobbies import MatchLobby
+from aoe2netwrapper.models.lobbies import MatchLobby as MatchLobby  # noqa
