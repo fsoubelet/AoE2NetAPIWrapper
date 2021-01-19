cc79ab905da4c472ae6c0f759afa2a5558ea1ab2
Felix Soubelet <felix.soubelet@protonmail.com>
2021-01-19 13:44:33 +0100

    tests for Convert

--- tests/test_converters.py
+++ tests/test_converters.py
@@ -1,8 +1,11 @@
 import json
 import pathlib
 
+import pandas as pd
 import pytest
+import responses
 
+from aoe2netwrapper import AoE2NetAPI
 from aoe2netwrapper.converters import Convert
 
 CURRENT_DIR = pathlib.Path(__file__).parent
@@ -84,7 +87,160 @@ class TestExceptions:
 
 
 class TestConvert:
-    pass
+    client = AoE2NetAPI()
+
+    @responses.activate
+    def test_strings(self, strings_defaults_payload, strings_converted):
+        responses.add(
+            responses.GET,
+            "https://aoe2.net/api/strings",
+            json=strings_defaults_payload,
+            status=200,
+        )
+
+        result = self.client.strings()
+        dframe = Convert.strings(result)
+
+        assert isinstance(dframe, pd.DataFrame)
+        assert dframe.size == 1331
+        assert dframe.shape == (121, 11)
+        pd.testing.assert_frame_equal(dframe, strings_converted)
+
+    @responses.activate
+    def test_leaderboard(self, leaderboard_defaults_payload, leaderboard_converted):
+        responses.add(
+            responses.GET,
+            "https://aoe2.net/api/leaderboard",
+            json=leaderboard_defaults_payload,
+            status=200,
+        )
+
+        result = self.client.leaderboard()
+        dframe = Convert.leaderboard(result)
+
+        assert isinstance(dframe, pd.DataFrame)
+        assert dframe.size == 230
+        assert dframe.shape == (10, 23)
+        pd.testing.assert_frame_equal(dframe, leaderboard_converted)
+
+    @responses.activate
+    def test_lobbies(self, lobbies_defaults_payload, lobbies_converted):
+        responses.add(
+            responses.GET,
+            "https://aoe2.net/api/lobbies",
+            json=lobbies_defaults_payload,
+            status=200,
+        )
+
+        result = self.client.lobbies()
+        dframe = Convert.lobbies(result)
+
+        assert isinstance(dframe, pd.DataFrame)
+        assert dframe.size == 34086
+        assert dframe.shape == (598, 57)
+        pd.testing.assert_frame_equal(dframe, lobbies_converted)
+
+    @responses.activate
+    def test_last_match_endpoint_with_steamid(self, last_match_steamid_payload, last_match_converted):
+        responses.add(
+            responses.GET,
+            "https://aoe2.net/api/player/lastmatch",
+            json=last_match_steamid_payload,
+            status=200,
+        )
+
+        result = self.client.last_match(steam_id=76561199003184910)
+        dframe = Convert.last_match(result)
+
+        assert isinstance(dframe, pd.DataFrame)
+        assert dframe.size == 45
+        assert dframe.shape == (1, 45)
+        pd.testing.assert_frame_equal(dframe, last_match_converted)
+
+    @responses.activate
+    def test_match_history(self, match_history_steamid_payload, match_history_converted):
+        responses.add(
+            responses.GET,
+            "https://aoe2.net/api/player/matches",
+            json=match_history_steamid_payload,
+            status=200,
+        )
+
+        result = self.client.match_history(steam_id=76561199003184910)
+        dframe = Convert.match_history(result)
+
+        assert isinstance(dframe, pd.DataFrame)
+        assert dframe.size == 1482
+        assert dframe.shape == (26, 57)
+        pd.testing.assert_frame_equal(dframe, match_history_converted)
+
+    @responses.activate
+    def test_rating_history(self, rating_history_profileid_payload, rating_history_converted):
+        responses.add(
+            responses.GET,
+            "https://aoe2.net/api/player/ratinghistory",
+            json=rating_history_profileid_payload,
+            status=200,
+        )
+
+        result = self.client.rating_history(profile_id=459658)
+        dframe = Convert.rating_history(result)
+
+        assert isinstance(dframe, pd.DataFrame)
+        assert dframe.size == 600
+        assert dframe.shape == (100, 6)
+        pd.testing.assert_frame_equal(dframe, rating_history_converted)
+
+    @responses.activate
+    def test_matches(self, matches_defaults_payload, matches_converted):
+        responses.add(
+            responses.GET,
+            "https://aoe2.net/api/matches",
+            json=matches_defaults_payload,
+            status=200,
+        )
+
+        result = self.client.matches()
+        dframe = Convert.matches(result)
+
+        assert isinstance(dframe, pd.DataFrame)
+        assert dframe.size == 2622
+        assert dframe.shape == (46, 57)
+        pd.testing.assert_frame_equal(dframe, matches_converted)
+
+    @responses.activate
+    def test_match(self, match_uuid_payload, match_converted):
+        responses.add(
+            responses.GET,
+            "https://aoe2.net/api/match",
+            json=match_uuid_payload,
+            status=200,
+        )
+
+        result = self.client.match(uuid="66ec2575-5ee4-d241-a1fc-d7ffeffb48b6")
+        dframe = Convert.match(result)
+
+        assert isinstance(dframe, pd.DataFrame)
+        assert dframe.size == 456
+        assert dframe.shape == (8, 57)
+        pd.testing.assert_frame_equal(dframe, match_converted)
+
+    @responses.activate
+    def test_num_online(self, num_online_defaults_payload, num_online_converted):
+        responses.add(
+            responses.GET,
+            "https://aoe2.net/api/stats/players",
+            json=num_online_defaults_payload,
+            status=200,
+        )
+
+        result = self.client.num_online()
+        dframe = Convert.num_online(result)
+
+        assert isinstance(dframe, pd.DataFrame)
+        assert dframe.size == 5752
+        assert dframe.shape == (719, 8)
+        pd.testing.assert_frame_equal(dframe, num_online_converted)
 
 
 # ----- Fixtures ----- #
@@ -99,6 +255,12 @@ def strings_defaults_payload() -> dict:
 
 
 @pytest.fixture()
+def strings_converted() -> pd.DataFrame:
+    strings_converted_file = INPUTS_DIR / "convert_strings.pkl"
+    return pd.read_pickle(strings_converted_file)
+
+
+@pytest.fixture()
 def leaderboard_defaults_payload() -> dict:
     leaderboard_defaults_file = INPUTS_DIR / "leaderboard_defaults.json"
     with leaderboard_defaults_file.open("r") as fileobj:
@@ -107,27 +269,9 @@ def leaderboard_defaults_payload() -> dict:
 
 
 @pytest.fixture()
-def leaderboard_search_payload() -> dict:
-    leaderboard_search_file = INPUTS_DIR / "leaderboard_search.json"
-    with leaderboard_search_file.open("r") as fileobj:
-        payload = json.load(fileobj)
-    return payload
-
-
-@pytest.fixture()
-def leaderboard_steamid_payload() -> dict:
-    leaderboard_steamid_file = INPUTS_DIR / "leaderboard_steamid.json"
-    with leaderboard_steamid_file.open("r") as fileobj:
-        payload = json.load(fileobj)
-    return payload
-
-
-@pytest.fixture()
-def leaderboard_profileid_payload() -> dict:
-    leaderboard_profileid_file = INPUTS_DIR / "leaderboard_profileid.json"
-    with leaderboard_profileid_file.open("r") as fileobj:
-        payload = json.load(fileobj)
-    return payload
+def leaderboard_converted() -> pd.DataFrame:
+    leaderboard_converted_file = INPUTS_DIR / "convert_leaderboard.pkl"
+    return pd.read_pickle(leaderboard_converted_file)
 
 
 @pytest.fixture()
@@ -139,6 +283,12 @@ def lobbies_defaults_payload() -> dict:
 
 
 @pytest.fixture()
+def lobbies_converted() -> pd.DataFrame:
+    lobbies_converted_file = INPUTS_DIR / "convert_lobbies.pkl"
+    return pd.read_pickle(lobbies_converted_file)
+
+
+@pytest.fixture()
 def last_match_steamid_payload() -> dict:
     last_match_steamid_file = INPUTS_DIR / "last_match_steamid.json"
     with last_match_steamid_file.open("r") as fileobj:
@@ -147,11 +297,9 @@ def last_match_steamid_payload() -> dict:
 
 
 @pytest.fixture()
-def last_match_profileid_payload() -> dict:
-    last_match_profileid_file = INPUTS_DIR / "last_match_profileid.json"
-    with last_match_profileid_file.open("r") as fileobj:
-        payload = json.load(fileobj)
-    return payload
+def last_match_converted() -> pd.DataFrame:
+    last_match_converted_file = INPUTS_DIR / "convert_last_match.pkl"
+    return pd.read_pickle(last_match_converted_file)
 
 
 @pytest.fixture()
@@ -163,19 +311,9 @@ def match_history_steamid_payload() -> dict:
 
 
 @pytest.fixture()
-def match_history_profileid_payload() -> dict:
-    match_history_profileid_file = INPUTS_DIR / "match_history_profileid.json"
-    with match_history_profileid_file.open("r") as fileobj:
-        payload = json.load(fileobj)
-    return payload
-
-
-@pytest.fixture()
-def rating_history_steamid_payload() -> dict:
-    rating_history_steamid_file = INPUTS_DIR / "rating_history_steamid.json"
-    with rating_history_steamid_file.open("r") as fileobj:
-        payload = json.load(fileobj)
-    return payload
+def match_history_converted() -> pd.DataFrame:
+    match_history_converted_file = INPUTS_DIR / "convert_match_history.pkl"
+    return pd.read_pickle(match_history_converted_file)
 
 
 @pytest.fixture()
@@ -187,6 +325,12 @@ def rating_history_profileid_payload() -> dict:
 
 
 @pytest.fixture()
+def rating_history_converted() -> pd.DataFrame:
+    rating_history_converted_file = INPUTS_DIR / "convert_rating_history.pkl"
+    return pd.read_pickle(rating_history_converted_file)
+
+
+@pytest.fixture()
 def matches_defaults_payload() -> dict:
     matches_defaults_file = INPUTS_DIR / "matches_defaults.json"
     with matches_defaults_file.open("r") as fileobj:
@@ -195,11 +339,9 @@ def matches_defaults_payload() -> dict:
 
 
 @pytest.fixture()
-def matches_since_payload() -> dict:
-    matches_since_file = INPUTS_DIR / "matches_since.json"
-    with matches_since_file.open("r") as fileobj:
-        payload = json.load(fileobj)
-    return payload
+def matches_converted() -> pd.DataFrame:
+    matches_converted_file = INPUTS_DIR / "convert_matches.pkl"
+    return pd.read_pickle(matches_converted_file)
 
 
 @pytest.fixture()
@@ -211,11 +353,9 @@ def match_uuid_payload() -> dict:
 
 
 @pytest.fixture()
-def match_matchid_payload() -> dict:
-    match_match_id_file = INPUTS_DIR / "match_matchid.json"
-    with match_match_id_file.open("r") as fileobj:
-        payload = json.load(fileobj)
-    return payload
+def match_converted() -> pd.DataFrame:
+    match_converted_file = INPUTS_DIR / "convert_match.pkl"
+    return pd.read_pickle(match_converted_file)
 
 
 @pytest.fixture()
@@ -224,3 +364,9 @@ def num_online_defaults_payload() -> dict:
     with num_online_defaults_file.open("r") as fileobj:
         payload = json.load(fileobj)
     return payload
+
+
+@pytest.fixture()
+def num_online_converted() -> pd.DataFrame:
+    num_online_converted_file = INPUTS_DIR / "convert_num_online.pkl"
+    return pd.read_pickle(num_online_converted_file)
