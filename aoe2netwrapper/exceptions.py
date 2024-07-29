2bad1be7706aae56f6b36ef39f30ddd5b71bd75d
Felix Soubelet <felix.soubelet@protonmail.com>
2024-07-29 13:50:23 +0200

    add specific errors for removed endpoints

--- aoe2netwrapper/exceptions.py
+++ aoe2netwrapper/exceptions.py
@@ -9,5 +9,13 @@ class Aoe2NetError(Exception):
     """Default exception for AoE2.net API interaction."""
 
 
+class RemovedApiEndpointError(Aoe2NetError):
+    """Exception raised when an API endpoint is removed from AoE2.net."""
+
+    def __init__(self, endpoint: str) -> None:
+        msg = f"The API endpoint for '{endpoint}' has been removed from AoE2.net."
+        super().__init__(msg)
+
+
 class NightBotError(Exception):
     """Default exception for AoE2.net Nightbot API interaction."""
