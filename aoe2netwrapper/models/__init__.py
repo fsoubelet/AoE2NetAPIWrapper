e504e1eeea26640d81ddfd9c806c7588e8507304
Felix Soubelet <felix.soubelet@protonmail.com>
2024-07-29 11:35:13 +0200

    fix TID252

--- aoe2netwrapper/models/__init__.py
+++ aoe2netwrapper/models/__init__.py
@@ -5,12 +5,11 @@ aoe2netwrapper.models
 This subpackage contains the model objects used to encapsulate responses from the API.
 Each module therein contains the models for a specific API endpoint.
 """
-from .last_match import LastMatchResponse
-from .leaderboard import LeaderBoardResponse
-from .lobbies import MatchLobby
-from .match import MatchLobby
-from .match_history import MatchLobby
-from .matches import MatchLobby
-from .num_online import NumOnlineResponse
-from .rating_history import RatingTimePoint
-from .strings import StringsResponse
+from .last_match import LastMatchResponse  # noqa: TID252
+from .leaderboard import LeaderBoardResponse  # noqa: TID252
+from .lobbies import MatchLobby  # noqa: TID252
+from .num_online import NumOnlineResponse  # noqa: TID252
+from .rating_history import RatingTimePoint  # noqa: TID252
+from .strings import StringsResponse  # noqa: TID252
+
+__all__ = ["LastMatchResponse", "LeaderBoardResponse", "MatchLobby", "NumOnlineResponse", "RatingTimePoint", "StringsResponse"]
